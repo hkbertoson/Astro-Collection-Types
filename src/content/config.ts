4387cecda0bbf70632a25a46acf672cd7288c2f9
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-10-03 11:14:50 -0500

    Types

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,26 +1,26 @@
-import { defineCollection } from 'astro:content';
-import { z } from 'astro:schema';
+import {defineCollection} from 'astro:content';
+import {z} from 'astro:schema';
 
 const firstName = defineCollection({
-  schema: z.object({
-    firstName: z.string(),
-  }),
+	schema: z.object({
+		name: z.string(),
+	}),
 });
 
 const middleName = defineCollection({
-  schema: z.object({
-    middleName: z.string(),
-  }),
+	schema: z.object({
+		name: z.string(),
+	}),
 });
 
 const lastName = defineCollection({
-  schema: z.object({
-    lastName: z.string(),
-  }),
+	schema: z.object({
+		name: z.string(),
+	}),
 });
 
 export const collections = {
-  firstName,
-  middleName,
-  lastName,
+	firstName,
+	middleName,
+	lastName,
 };
